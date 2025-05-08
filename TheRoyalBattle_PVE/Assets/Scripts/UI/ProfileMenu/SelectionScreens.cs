26ebe022f0e5cf725b601dbc1b73d5bcd3f4827f
Anirudh <57024963+ILLUTE@users.noreply.github.com>
2025-05-08 23:19:49 +0530

    Delete TheRoyalBattle_PVE directory

--- TheRoyalBattle_PVE/Assets/Scripts/UI/ProfileMenu/SelectionScreens.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class SelectionScreens : MonoBehaviour
-{
-    public abstract void OnScreenOpen();
-    public abstract void OnScreenClose();
-}
